6e952cf2bca89250b2ae43de7a13726b8b61becb
miaoyin <miaoyin@woda.com>
2023-07-13 13:39:50 +0800

    fix: 子进程同时关闭

--- watch/service.go
+++ watch/service.go
@@ -3,8 +3,12 @@ package watch
 import (
 	"context"
 	"errors"
+	"io/fs"
 	"os"
 	"os/exec"
+	"os/signal"
+	"path/filepath"
+	"syscall"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
@@ -15,31 +19,56 @@ import (
 type Service struct {
 	watcher *fsnotify.Watcher
 	cancel  context.CancelFunc
+	pid     int
 }
 
 func NewService() *Service {
-	return &Service{lo.Must1(fsnotify.NewWatcher()), nil}
+	return &Service{lo.Must1(fsnotify.NewWatcher()), nil, 0}
 }
 
 func (p *Service) Add(path string) {
-	lo.Must0(p.watcher.Add(path))
+	lo.Must0(filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return p.watcher.Add(path)
+		}
+
+		return nil
+	}))
 }
 
-func (p *Service) Run(name string, args []string) {
+func (p *Service) Run(cmd string, args []string) {
 	go p.watch()
+	go p.run(cmd, args)
 
+	osc := make(chan os.Signal, 1)
+
+	signal.Notify(osc, syscall.SIGTERM, syscall.SIGINT)
+
+	num := <-osc
+
+	p.Close()
+	logs.D.Println("监听到退出信号:", num)
+}
+
+func (p *Service) run(name string, args []string) {
 	for {
 		ctx, can := context.WithCancel(context.Background())
-		com := exec.CommandContext(ctx, name, args...)
-		com.Stdin = os.Stdin
-		com.Stdout = os.Stdout
+		cmd := exec.CommandContext(ctx, name, args...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 		p.cancel = can
 
-		lo.Must0(com.Start())
+		lo.Must0(cmd.Start())
+		p.pid = cmd.Process.Pid
 
-		if err := com.Wait(); err.Error() == "signal: killed" || errors.Is(err, context.Canceled) {
+		if err := cmd.Wait(); err != nil && (err.Error() == "signal: killed" || errors.Is(err, context.Canceled)) {
+			lo.Must0(syscall.Kill(-p.pid, syscall.SIGKILL))
 			time.Sleep(time.Second)
 		} else {
+			logs.Log(err)
+
 			return
 		}
 	}
@@ -77,4 +106,8 @@ func (p *Service) watch() {
 
 func (p *Service) Close() {
 	p.watcher.Close()
+
+	if p.pid > 0 {
+		lo.Must0(syscall.Kill(-p.pid, syscall.SIGKILL))
+	}
 }
